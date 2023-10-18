44ad5e2e1dc95bd5cb3af62a470747e2da30e3ea
Andrey Maksimov <andrey@tenderowl.com>
2023-10-18 16:24:54 +0300

    Add logger and env vars

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::net::SocketAddr;
+use log::info;
 use tonic::transport::Server;
 
 use agreements::agreements_service_server::AgreementsServiceServer;
@@ -11,8 +13,12 @@ pub mod agreements {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let addr = "127.0.0.1:50051".parse()?;
+    env_logger::init();
+
+    let addr = std::env::var("APP_ADDRESS").unwrap_or("127.0.0.1:50051".to_string()).parse()?;
     let agreementer = Agreementer::default();
+
+    info!("Starting gRPC Server at {}", addr);
 
     Server::builder()
         .add_service(AgreementsServiceServer::new(agreementer))
--- src/services/agreements.rs
+++ src/services/agreements.rs
@@ -1,5 +1,7 @@
+use log::debug;
 use tonic::{Request, Response, Status};
 use tonic::codegen::tokio_stream::wrappers::ReceiverStream;
+
 use crate::agreements::{Agreement, AgreementAcceptance, AgreementAcceptanceResponse};
 use crate::agreements::agreements_service_server::AgreementsService;
 
@@ -9,28 +11,34 @@ pub struct Agreementer;
 #[tonic::async_trait]
 impl AgreementsService for Agreementer {
     async fn create_agreement(&self, request: Request<Agreement>) -> Result<Response<Agreement>, Status> {
+        debug!("Called create_agreement");
         todo!()
     }
 
     async fn update_agreement(&self, request: Request<Agreement>) -> Result<Response<Agreement>, Status> {
+        debug!("Called update_agreement");
         todo!()
     }
 
     async fn delete_agreement(&self, request: Request<Agreement>) -> Result<Response<Agreement>, Status> {
+        debug!("Called delete_agreement");
         todo!()
     }
 
     async fn get_agreement(&self, request: Request<Agreement>) -> Result<Response<Agreement>, Status> {
+        debug!("Called get_agreement");
         todo!()
     }
 
     type GetAgreementAcceptancesStream = ReceiverStream<Result<AgreementAcceptance, Status>>;
 
     async fn get_agreement_acceptances(&self, request: Request<Agreement>) -> Result<Response<Self::GetAgreementAcceptancesStream>, Status> {
+        debug!("Called get_agreement_acceptances");
         todo!()
     }
 
     async fn set_agreement_acceptance(&self, request: Request<AgreementAcceptance>) -> Result<Response<AgreementAcceptanceResponse>, Status> {
+        debug!("Called set_agreement_acceptance");
         todo!()
     }
 }
